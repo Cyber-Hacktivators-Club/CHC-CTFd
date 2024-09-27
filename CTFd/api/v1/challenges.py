0dcea6bb82783d3cb4cc030cf9604336e1db1153
Haseeb Shaikh <106930649+CloudMeshSolutions@users.noreply.github.com>
2024-09-28 01:58:45 +0500

    tweaked challenges/types endpoint

--- CTFd/api/v1/challenges.py
+++ CTFd/api/v1/challenges.py
@@ -266,7 +266,8 @@ class ChallengeTypes(Resource):
                 "templates": challenge_class.templates,
                 "scripts": challenge_class.scripts,
                 "create": render_template(
-                    challenge_class.templates["create"].lstrip("/")
+                    challenge_class.templates["create"].lstrip("/"),
+                    challenge=challenge_class
                 ),
             }
         return {"success": True, "data": response}
